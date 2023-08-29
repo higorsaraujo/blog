7e993bd76f1c70a560130fad10ca18a6b0ad2126
Higor <higor.a@aluno.ifsp.edu.br>
2023-08-29 10:43:42 -0300

    jjhkh

--- includes/menu.php
+++ includes/menu.php
@@ -16,12 +16,12 @@
             <li class="nav-item">
                 <a class="nav-link" href="post_formulario.php">Incluir Post</a>
             </li>
-            <?php if ((isset($_SESSION['login'])))
+            <?php if ((isset($_SESSION['login']))
                         && ($_SESSION['login']['usuario']['adm'] === 1)) : ?>
             <li class="nav-item">
                 <a class="nav-link" href="usuarios.php">Usuarios</a>
             </li>
-            <?php endif; ?>
+            <?php endif;?>
         </ul>
     </div>
 </div>
